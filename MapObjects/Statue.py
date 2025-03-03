e8bdd923db846ca6564953753e88fb203538dc77
tarunk345 <tarun.kalyanaraman@mail.mcgill.ca>
2025-03-03 16:18:45 -0500

    deleted MapObjects folder and added all the classes to maze_objects.py

--- MapObjects/Statue.py
+++
@@ -1,8 +0,0 @@
-from tiles.base import MapObject
-
-class Statue(MapObject):
-    #Statue
-
-    def __init__(self, image_name: str = "statue"):
-        #Constructor for new statue
-        super().__init__(f'tile/statue/{image_name}', passable=False)
--- maze_objects.py
+++ maze_objects.py
@@ -10,18 +10,66 @@ if TYPE_CHECKING:
     from maps.base import Map
 
 
-class Room(MapObject):
-    def __init__(self, image_name: str, passable: bool = True, puzzle: str = None):
-        super().__init__(image_name, passable)
-        self.puzzle = puzzle
+class Room(Map): 
+    #A generic room that can contain objects and players
 
+    def __init__(self, name: str, size: tuple[int, int], entry_point: Coord, background_tile: str = "floor"):
+        #Constructor for new room
+        super().__init__(name=name, description="", size=size, entry_point=entry_point, background_tile_image=background_tile)
+        self.objects: list[tuple[MapObject, Coord]] = []
+
+    def add_object(self, obj: MapObject, position: Coord) -> None:
+        #Add a new object in the room
+        self.objects.append((obj, position))
+
+    def get_objects(self) -> list[tuple[MapObject, Coord]]:
+        #Return all objects in the room
+        return self.objects
+     
     @abstractmethod
     def player_entered(self, player: "HumanPlayer") -> list[Message]:
         return []
+    
+class Statue(MapObject):
+    #Statue
+
+    def __init__(self, image_name: str = "statue"):
+        #Constructor for new statue
+        super().__init__(f'tile/statue/{image_name}', passable=False)
+
+        
+class Corridor(Room):
+    #Creates a new corridor
+
+    def __init__(self, name: str, length: int, entry_point: Coord):
+        #Cunstroctor for new corridor
+        super().__init__(name=name, size=(1, length), entry_point=entry_point, background_tile="stone")
+
+class Door(MapObject):
+    #A door that connects rooms.
+
+    def __init__(self, image_name: str, linked_room: str = ""):
+        super().__init__(f'tile/door/{image_name}', passable=True, z_index=0)
+        self.__connected_room = None
+        self.__linked_room = linked_room
+
+    def connect_to(self, connected_room: Room, new_entry_point: Coord) -> None:
+        #Links this door to another room.
+        self.__connected_room = connected_room
+        self.__new_entry_point = new_entry_point
+
+    def player_entered(self, player) -> list:
+        #Moves player to linked room if connected.
+        if self.__connected_room is None or self.__new_entry_point is None:
+            print("Door has no link")
+            return []
+
+        return player.change_room(self.__connected_room, entry_point=self.__new_entry_point)
+
 
 class SaunaRoom(Room):
-    def __init__(self, image_name: str, passable: bool = True, heat_level: int):
-        super().__init__(image_name, passable)
+    def __init__(self, heat_level: int, name: str, size: tuple[int, int], entry_point: Coord, background_tile: str = "floor"):
+        super().__init__(name=name, size=size, entry_point=entry_point, background_tile=background_tile)
         self.heat_level = heat_level
 
     def player_entered(self, player: "HumanPlayer") -> list[Message]:
@@ -40,16 +88,15 @@ class SaunaRoom(Room):
     #add a taking damage method in here
 
 class StatueRoom(Room):
-    def __init__(self, image_name: str, passable: bool = True):
-        super().__init__(image_name, passable)
+    def __init__(self, name: str, size: tuple[int, int], entry_point: Coord, background_tile: str = "floor"):
+        super().__init__(name=name, size=size, entry_point=entry_point, background_tile=background_tile)
 
     def player_entered(self, player: "HumanPlayer") -> list[Message]:
         return [ServerMessage(player, f"You have entered the Room of Statues.")]
 
 class WineCellar(Room):
-    def __init__(self, image_name: str, passable: bool = True, potion: Potion):
-        super().__init__(image_name, passable)
-        self.potion = potion;
+    def __init__(self, name: str, size: tuple[int, int], entry_point: Coord, background_tile: str = "floor"):
+        super().__init__(name=name, size=size, entry_point=entry_point, background_tile=background_tile)
 
     def player_entered(self, player: "HumanPlayer") -> list[Message]:
         return [ServerMessage(player, f"You have entered the Wine Cellar.")]
