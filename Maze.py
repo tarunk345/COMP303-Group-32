9e5a032bbbdf6c272062e37c4fef67a4f28b76ac
tarunk345 <tarun.kalyanaraman@mail.mcgill.ca>
2025-03-19 21:23:10 -0400

    changes that got client_local to display maze room!! Changed Room class structure, and maze entry

--- Maze.py
+++ Maze.py
@@ -1,39 +1,41 @@
 from typing import TYPE_CHECKING
 from PIL import Image
-from .maze_objects import *
 from .imports import * 
+
+from .maze_objects import *
 if TYPE_CHECKING:
+    from resources import Resources
     from coord import Coord
     from maps.base import Map
     from tiles.base import MapObject
     from tiles.map_objects import *
 
-
 class Maze(Map):
     def __init__(self) -> None:
         super().__init__(
-            name="ExampleHouse",
+            name="Maze",
             description="",
             size=(73, 73),
-            entry_point=Coord(17, 14),
+            entry_point=Coord(17,14),
             background_tile_image='cobblestone',
         )
     
     def get_objects(self) -> list[tuple[MapObject, Coord]]:
         objects: list[tuple[MapObject, Coord]] = []
-
+        # creates maze base with walls
+        self.__create_maze_base()
         # add a door
         door = Door('int_entrance', linked_room="Trottier Town")
-        objects.append((door, Coord(63,0)))
+        objects.append((door, Coord(72,63)))
 
-        door2 = Door('int_entrance', linked_room="Trottier Town")
-        objects.append((door2,Coord(23,71)))
+        #door2 = Door('int_entrance', linked_room="Trottier Town")
+        #objects.append((door2,Coord(23,71)))
 
         # add a statue
         return objects
     
-    def create_maze_base(self):
-        image = Image.open(get_resource_path("maze_template", ext_folder=True))
+    def __create_maze_base(self):
+        image = Image.open(Resources.get_resource_path("maze_template", ext_folder=True))
         
         for y in range(73):
             for x in range(73):
