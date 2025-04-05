bfb0a6c173646e3c82139f57477cae90e35c2f00
tanav1302 <tanav.bansal@mail.mcgill.ca>
2025-04-05 13:26:46 -0400

    merge

--- Defense.py
+++ Defense.py
@@ -5,14 +5,23 @@ import random
 
 
 
-from .imports import * 
 
+
+
+
+
+from .imports import * 
 if TYPE_CHECKING:
     from coord import Coord
+    from Player import HumanPlayer
     from tiles.base import MapObject
     from message import *
     from .Maze import ExampleHouse
-    from message import Message
+    
+
+
+
+
 
 
 
@@ -24,6 +33,9 @@ class Defense_type(Enum):
     BOOTS = 4
     DEFENSE_POTION = 5
     ATTACK_POTION = 6
+
+
+
 
 
 
@@ -50,9 +62,14 @@ class Defense(ABC):
 
 
 
+
+
+
+
+
 class Armor(Defense, MapObject):
     @abstractmethod
-    def __init__(self, defense_value:int, attack_value:int,defense_type:Defense_type,player:"maze_player", maze_position:Coord, 
+    def __init__(self, defense_value:int, attack_value:int,defense_type:Defense_type,player:"maze_player", 
                  maze: "ExampleHouse", image_name: str, passable: bool = True, z_index: int = 0) -> None:
         super().__init__(image_name, passable, z_index)
         self.__defense_value:int = defense_value
@@ -60,14 +77,10 @@ class Armor(Defense, MapObject):
         self.__defense_type:Defense_type = defense_type
         self.__maze:"ExampleHouse" = maze
         self.__player:"maze_player" = player
-        self.__maze_position:Coord = maze_position
     
     def __str__(self)->str:
         return self.get_image_name()
     
-    def get_maze_position(self) -> Coord:
-        return self.__maze_position
-
     def get_defense_value(self) -> int:
         return self.__defense_value
     
@@ -77,7 +90,7 @@ class Armor(Defense, MapObject):
     def get_defense_type(self)->Defense_type:
         return self.__defense_type
     
-    def player_entered(self, player: "HumanPlayer") -> list[Message]:
+    def player_interacted(self, player: HumanPlayer) -> list[Message]:
         """if armor added then update attack value of player and return pick text
             if armor not added put the the armor back on grid and return not pick text
             if armor changed then put the old armor back on grid,update attack value of player and return change text"""
@@ -87,20 +100,20 @@ class Armor(Defense, MapObject):
             pick_text : str ='You picked up the '+ self.get_image_name() +'!\n Defense increased by: ' + str(self.__defense_value) +'!\n Attack increased by: ' + str(self.__attack_value)+'!'
             return [DialogueMessage(self, player, pick_text,self.get_image_name())]
         elif armor == self :
-            self.__maze.add_to_grid(self,self.__maze_position)
-            not_pick_text : str = 'current' + self.get_image_name() + 'has more defense!'
+            self.__maze.add_to_grid(self,player.get_current_position())
+            not_pick_text : str = 'current ' + self.get_image_name() + ' has more defense!'
             return [DialogueMessage(self, player, not_pick_text,self.get_image_name())]
         else:
             if(isinstance(armor,(Armor,Potion))): 
-                self.__maze.add_to_grid(armor,self.__maze_position)
+                self.__maze.add_to_grid(armor,player.get_current_position())
                 defense_changed: int = self.__defense_value - armor.__defense_value
                 attack_changed:int = self.__attack_value - armor.__attack_value
                 if defense_changed>=0 and attack_changed>=0:
-                    change_text = self.get_image_name() + 'changed! Defense' +'increased by' + str(defense_changed)+'\n' + 'attack increased by' + str(attack_changed)+'!'
+                    change_text = armor.get_image_name() + ' changed! Defense increased by ' + str(defense_changed)+'\n' + ' attack increased by ' + str(attack_changed)+'!'
                 elif defense_changed<0: 
-                    change_text = self.get_image_name() + 'changed! Defense' +'decreased by' + str(abs(defense_changed))+'\n' + 'attack increased by' + str(attack_changed)+'!'
+                    change_text = armor.get_image_name() + ' changed! Defense decreased by ' + str(abs(defense_changed))+'\n' + ' attack increased by ' + str(attack_changed)+'!'
                 elif attack_changed<0:
-                    change_text = self.get_image_name() + 'changed! Defense' +'increased by' + str(defense_changed)+'\n' + 'attack decreased by' + str(abs(attack_changed))+'!'
+                    change_text = armor.get_image_name() + ' changed! Defense increased by ' + str(defense_changed)+'\n' + ' attack decreased by ' + str(abs(attack_changed))+'!'
 
             return [DialogueMessage(self, player, change_text,self.get_image_name())]
 
@@ -117,25 +130,32 @@ class Armor(Defense, MapObject):
 
 
 class Pants(Armor):
-    def __init__(self, defense_value: int, attack_value: int, maze_position:Coord, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
-        super().__init__(defense_value, attack_value, Defense_type.PANTS, player,maze_position, maze, image_name)
+    def __init__(self, defense_value: int, attack_value: int, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
+        super().__init__(defense_value, attack_value, Defense_type.PANTS, player,maze, image_name)
     
 
 
 
 class Chest_Plate(Armor):
-    def __init__(self, defense_value: int, attack_value: int, maze_position:Coord, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
-        super().__init__(defense_value, attack_value, Defense_type.CHEST_PLATE, player,maze_position, maze, image_name)
+    def __init__(self, defense_value: int, attack_value: int, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
+        super().__init__(defense_value, attack_value, Defense_type.CHEST_PLATE, player,maze, image_name)
     
 
 
 class Helmet(Armor):
-    def __init__(self, defense_value: int, attack_value: int, maze_position:Coord, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
-        super().__init__(defense_value, attack_value, Defense_type.HELMET, player,maze_position, maze, image_name)
+    def __init__(self, defense_value: int, attack_value: int, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
+        super().__init__(defense_value, attack_value, Defense_type.HELMET, player,maze, image_name)
 
 class Boots(Armor):
-    def __init__(self, defense_value: int, attack_value: int, maze_position:Coord, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
-        super().__init__(defense_value, attack_value, Defense_type.BOOTS, player,maze_position, maze, image_name)
+    def __init__(self, defense_value: int, attack_value: int, player: "maze_player", maze: "ExampleHouse", image_name: str) -> None:
+        super().__init__(defense_value, attack_value, Defense_type.BOOTS, player,maze, image_name)
+
+
+
+
+
+
+
 
 
 
@@ -269,6 +289,11 @@ class Attack_potion(Potion):
 
 
 
+
+
+
+
+
 class Armor_Set(Defense):
     def __init__(self) -> None:
         super().__init__()
@@ -324,7 +349,7 @@ class Armor_Set(Defense):
                     else :
                         return armor
                 
-        
+        self.__list_armors.append(armor)
         return None
     
     def add_potion(self, potion : Potion)->Optional[Potion]  :
@@ -341,6 +366,25 @@ class Armor_Set(Defense):
         self.__list_armors.append(potion)
     
         return None
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
