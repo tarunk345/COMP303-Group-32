4adb16c49e092ceecdc016b29888f33af3c2618d
tanav1302 <tanav.bansal@mail.mcgill.ca>
2025-04-07 23:16:34 -0400

    change in message

--- Enemy.py
+++ Enemy.py
@@ -71,7 +71,7 @@ class Enemy(NPC, CharacterMapObject):
         if self.__defense == 0:
             # return [DialogueMessage(self, player, f"{self.get_name()} attacks you for {self.__attack_damage} damage!", self.get_image())]
             player.get_current_room().remove_from_grid(self,self.get_current_position())
-            return [DialogueMessage(self, player,self.get_name() + "died!",self.get_image_name())]
+            return [DialogueMessage(self, player,self.get_name() + "died!"+player.get_name() + "received damage! \n"+player.get_name()+"remaining Health:"+str(self.get_maze_player().get_defense_value()),self.get_image_name())]
         
         if self.attack() > 0:
             # player.change_room("Trottier Town")
@@ -80,7 +80,7 @@ class Enemy(NPC, CharacterMapObject):
             player.move_to(Coord(70,62))
             return [DialogueMessage(self, player,player.get_name() + "died!",self.get_image_name())]
 
-        return [DialogueMessage(self, player,self.get_name() + "received damage! \nremaining Health:"+str(self.__defense),self.get_image_name())] 
+        return [DialogueMessage(self, player,self.get_name() + "received damage! \n"+self.get_name()+"remaining Health:"+str(self.__defense)+"\n"+player.get_name() + "received damage! \n"+player.get_name()+"remaining Health:"+str(self.get_maze_player().get_defense_value()),self.get_image_name())] 
        
 class Minotaur(Enemy):
     _instance = None #Singleton
@@ -104,6 +104,25 @@ class Minotaur(Enemy):
             facing_direction='down',
             staring_distance=5
         )
+
+    def player_interacted(self, player: "HumanPlayer") -> List[Message]:
+        self.decrease_defense(self.__maze_player.get_attack_value())
+      
+        if self.__defense == 0:
+            # return [DialogueMessage(self, player, f"{self.get_name()} attacks you for {self.__attack_damage} damage!", self.get_image())]
+            player.get_current_room().remove_from_grid(self,self.get_current_position())
+            player.change_room(self.__maze_player.get_maze())
+            player.move_to(Coord(70,62))
+            return [DialogueMessage(self, player,self.get_name() + "died! You have cleared the Maze!",self.get_image_name())]
+        
+        if self.attack() > 0:
+            # player.change_room("Trottier Town")
+            # player.update_position(,self.__maze_player.get_maze())
+            player.change_room(self.__maze_player.get_maze())
+            player.move_to(Coord(70,62))
+            return [DialogueMessage(self, player,player.get_name() + "died!",self.get_image_name())]
+
+        return [DialogueMessage(self, player,self.get_name() + "received damage! \n"+self.get_name()+"remaining Health:"+str(self.__defense)+"\n"+player.get_name() + "received damage! \n"+player.get_name()+"remaining Health:"+str(self.get_maze_player().get_defense_value()),self.get_image_name())] 
 
     # def player_interacted(self, player: "HumanPlayer") -> List[DialogueMessage]:
     #     messages = [
