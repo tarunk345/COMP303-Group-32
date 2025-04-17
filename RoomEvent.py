dc7df83596a5552f90f8bf3ea40f6cf10a5d526e
tarunk345 <tarun.kalyanaraman@mail.mcgill.ca>
2025-04-17 13:46:53 -0400

    Update RoomEvent.py

--- RoomEvent.py
+++ RoomEvent.py
@@ -8,9 +8,3 @@ class RoomEvent(GameEvent):
 
     def __init__(self, event_type: str, data=None):
         super().__init__
-
-# class RoomEventsTypes:
-#     PLAYER_ENTERED = "player_entered"
-#     ENEMY_DEFEATED = "enemy_defeated"
-#     ALL_ENEMIES_DEFEATED = "all_enemies_defeated"
-#     DOOR_STATE_CHANGED = "door_state_changed"
